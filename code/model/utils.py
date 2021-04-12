847e5ccfc5c2b7580132665f69a622244f5237c3
ju-ki <juki.programming@gmail.com>
2021-04-12 14:22:50 +0900

    fix indent

--- code/model/utils.py
+++ code/model/utils.py
@@ -1,44 +1,37 @@
-import numpy as np
-import pandas as pd
-
 from sklearn.model_selection import KFold
 
 
 class GroupKFold:
 
-    """
-    ref:https://zenn.dev/mst8823/articles/cd40cb971f702e masatoさんのベースラインから
-    """
-  def __init__(self, n_splits=5, shuffle=True, random_state=0):
-    self.n_splits = n_splits
-    self.shuffle = shuffle
-    self.random_state = random_state
+    # ref:https://zenn.dev/mst8823/articles/cd40cb971f702e masatoさんのベースラインから
 
-  def get_n_splits(self, X=None, y=None, group=None):
-    return self.n_splits
+    def __init__(self, n_splits=5, shuffle=True, random_state=0):
+        self.n_splits = n_splits
+        self.shuffle = shuffle
+        self.random_state = random_state
 
-  def split(self, X=None, y=None, group=None):
-    kfold = KFold(n_splits=self.n_splits, shuffle=self.shuffle, random_state=self.random_state)
-    unique_ids = group.unique()
-    for tr_group_idx, va_group_idx in kfold.split(unique_ids):
-      tr_group, va_group = unique_ids[tr_group_idx], unique_ids[va_group_idx]
-      train_idx = np.where(group.isin(tr_group))[0]
-      val_idx = np.where(group.isin(va_group))[0]
-      yield train_idx, val_idx
-      
-      
+    def get_n_splits(self, X=None, y=None, group=None):
+        return self.n_splits
+
+    def split(self, X=None, y=None, group=None):
+        kfold = KFold(n_splits=self.n_splits, shuffle=self.shuffle,
+                      random_state=self.random_state)
+        unique_ids = group.unique()
+        for tr_group_idx, va_group_idx in kfold.split(unique_ids):
+            tr_group, va_group = unique_ids[tr_group_idx], unique_ids[va_group_idx]
+            train_idx = np.where(group.isin(tr_group))[0]
+            val_idx = np.where(group.isin(va_group))[0]
+            yield train_idx, val_idx
+
+
 def make_gkf(X, y, n_splits=5, random_state=0):
-   """
-   groupkfoldでcvを渡すための関数
-   予めgroup=data[target]としておく必要がある。
-   """ 
-    
-  gkf = GroupKFold(n_splits=n_splits, random_state=random_state)
-  return list(gkf.split(X, y, group))
+    #  groupkfoldでcvを渡すための関数
+    #  予めgroup=data[target]としておく必要がある。
+
+    gkf = GroupKFold(n_splits=n_splits, random_state=random_state)
+    return list(gkf.split(X, y, group))
+
 
 def make_folds(X, y, n_splits=5, random_state=0):
-　 """
-  kfoldでcvを渡すための関数
-  """
-  kfold = KFold(n_splits=n_splits, shuffle=True, random_state=random_state)
-  return list(kfold.split(X, y))
+    kfold = KFold(n_splits=n_splits, shuffle=True, random_state=random_state)
+    return list(kfold.split(X, y))
