84c8c7067cd47af9b51d631402066b0d728cfe98
ju-ki <juki.programming@gmail.com>
2021-11-09 16:51:56 +0900

    update project

---
+++ code/util/logger.py
@@ -0,0 +1,26 @@
+import os
+import datetime
+import logging
+
+
+class Logger:
+    """save log"""
+
+    def __init__(self, path, exp_name=None):
+        self.general_logger = logging.getLogger(path)
+        stream_handler = logging.StreamHandler()
+        file_general_handler = logging.FileHandler(
+            os.path.join(path, f'{exp_name}.log'))
+        if len(self.general_logger.handlers) == 0:
+            self.general_logger.addHandler(stream_handler)
+            self.general_logger.addHandler(file_general_handler)
+            self.general_logger.setLevel(logging.INFO)
+
+    def info(self, message):
+        # display time
+        self.general_logger.info(
+            '[{}] - {}'.format(self.now_string(), message))
+
+    @staticmethod
+    def now_string():
+        return str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
