61030b857c6df3c2aa635cc181b89c5d33d5c7d9
ju-ki <juki.programming@gmail.com>
2022-04-10 21:49:28 +0900

    fix script

--- jukijuki/tabular/util.py
+++ jukijuki/tabular/util.py
@@ -56,12 +56,12 @@ def run_blocks(input_df: pd.DataFrame, blocks: List, y=None, preprocess_block=No
     with Timer(logger=logger, prefix=f'create {task} block'):
         for block in blocks:
             if save_feature:
-                try:
-                    if hasattr(block, "cols"):
-                        if hasattr(block, "name") and hasattr(block, "n_component"):
-                            file_name = os.path.join(filepath + "features/", f"{task}_{block.__class__.__name__}_{str(block.cols)}_{str(block.name)}_{str(block.n_components)}.pkl")        
+                if hasattr(block, "cols"):
+                    if hasattr(block, "name") and hasattr(block, "n_component"):
+                        file_name = os.path.join(filepath + "features/", f"{task}_{block.__class__.__name__}_{str(block.cols)}_{str(block.name)}_{str(block.n_components)}.pkl")
+                    else:
                         file_name = os.path.join(filepath + "features/", f"{task}_{block.__class__.__name__}_{str(block.cols)}.pkl")
-                except:
+                else:
                     file_name = os.path.join(filepath + "features/", f"{task}_{block.__class__.__name__}.pkl")
 
             with Timer(logger=logger, prefix='\t- {}'.format(str(block))):
