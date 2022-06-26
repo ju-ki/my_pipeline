cd81bf2a85b1e8bfa8702ed2725979ea0b243778
ju-ki <juki.programming@gmail.com>
2022-06-26 21:45:17 +0900

    fix util.py bug and fix nlp_dataset

--- jukijuki/nlp/nlp_dataset.py
+++ jukijuki/nlp/nlp_dataset.py
@@ -3,14 +3,46 @@ from torch.utils.data import Dataset
 
 
 class SimpleTrainDataset(Dataset):
-    def __init__(self, df, config, tokenizer):
+    def __init__(self, df, config):
         assert hasattr(config, "sentence_col"), "Please create sentence_col attribute"
         assert hasattr(config, "target_col"), "Please create target_col attribute"
+        assert hasattr(config, "task"), "Please create task attribute(class, regression)"
+        assert hasattr(config, "tokenizer"), "Please create tokenizer attribute"
         self.df = df
         self.config = config
         self.sentence = self.df[self.config.sentence_col].to_numpy()
         self.target = self.df[self.config.target_col].to_numpy()
-        self.tokenizer = tokenizer
+        self.tokenizer = self.config.tokenizer
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, idx):
+        sentence = self.sentence[idx]
+        bert_sentence = self.tokenizer.encode_plus(
+            text=sentence,
+            add_special_tokens=True,
+            max_length=self.config.max_len,
+            truncation=True,)
+        if self.config.task == "class":
+            target = torch.tensor(self.target[idx], dtype=torch.long)
+        elif self.config.task == "regression":
+            target = torch.tensor(self.target[idx], dtype=torch.float)
+        return {
+            'input_ids': torch.tensor(bert_sentence["input_ids"], dtype=torch.long),
+            'attention_mask': torch.tensor(bert_sentence["attention_mask"], dtype=torch.long),
+            'target': target
+        }
+
+
+class SimpleTestDataset(Dataset):
+    def __init__(self, df, config):
+        assert hasattr(config, "sentence_col"), "Please create sentence_col attribute"
+        assert hasattr(config, "tokenizer"), "Please create tokenizer attribute"
+        self.df = df
+        self.config = config
+        self.sentence = self.df[self.config.sentence_col].to_numpy()
+        self.tokenizer = self.config.tokenizer
 
     def __len__(self):
         return len(self.df)
@@ -25,33 +57,7 @@ class SimpleTrainDataset(Dataset):
         return {
             'input_ids': torch.tensor(bert_sentence["input_ids"], dtype=torch.long),
             'attention_mask': torch.tensor(bert_sentence["attention_mask"], dtype=torch.long),
-            'target': torch.tensor(self.target[idx], dtype=torch.long)
         }
-
-
-class SimpleTestDataset(Dataset):
-    def __init__(self, df, config, tokenizer):
-        assert hasattr(config, "sentence_col"), "Please create sentence_col attribute"
-        self.df = df
-        self.config = config
-        self.sentence = self.df[self.config.sentence_col].to_numpy()
-        self.tokenizer = tokenizer
-
-    def __len__(self):
-        return len(self.df)
-
-    def __getitem__(self, idx):
-        sentence = self.sentence[idx]
-        bert_sentence = self.tokenizer.encode_plus(
-            text=sentence,
-            add_special_tokens=True,
-            max_length=self.config.max_len,
-            padding="max_length",
-            truncation=True,
-            return_attention_mask=True)
-        input_ids = torch.tensor(bert_sentence["input_ids"], dtype=torch.long)
-        attention_mask = torch.tensor(bert_sentence["attention_mask"], dtype=torch.long)
-        return input_ids, attention_mask
 
 
 def convert_examples_to_features(data, tokenizer, max_len,):
@@ -74,14 +80,16 @@ def convert_examples_to_features(data, tokenizer, max_len,):
 
 
 class ConvertTrainDataset(Dataset):
-    def __init__(self, df, config, tokenizer):
+    def __init__(self, df, config):
         assert hasattr(config, "sentence_col"), "Please create sentence_col attribute"
         assert hasattr(config, "target_col"), "Please create target_col attribute"
+        assert hasattr(config, "task"), "Please create task attribute(class, regression)"
+        assert hasattr(config, "tokenizer"), "Please create tokenizer attribute"
         self.df = df
         self.config = config
         self.sentence = self.df[self.config.sentence_col].to_numpy()
         self.target = self.df[self.config.target_col].to_numpy()
-        self.tokenizer = tokenizer
+        self.tokenizer = self.config.tokenizer
 
     def __len__(self):
         return len(self.df)
@@ -93,19 +101,25 @@ class ConvertTrainDataset(Dataset):
             tokenizer=self.tokenizer,
             max_len=self.config.max_len
         )
-        input_ids = torch.tensor(bert_sentence["input_ids"], dtype=torch.long)
-        attention_mask = torch.tensor(bert_sentence["attention_mask"], dtype=torch.long)
-        target = torch.tensor(self.target[idx], dtype=torch.float)
-        return input_ids, attention_mask, target
+        if self.config.task == "class":
+            target = torch.tensor(self.target[idx], dtype=torch.long)
+        elif self.config.task == "regression":
+            target = torch.tensor(self.target[idx], dtype=torch.float)
+        return {
+            'input_ids': torch.tensor(bert_sentence["input_ids"], dtype=torch.long),
+            'attention_mask': torch.tensor(bert_sentence["attention_mask"], dtype=torch.long),
+            'target': target
+        }
 
 
 class ConvertTestDataset(Dataset):
-    def __init__(self, df, config, tokenizer):
+    def __init__(self, df, config):
         assert hasattr(config, "sentence_col"), "Please create sentence_col attribute"
+        assert hasattr(config, "tokenizer"), "Please create tokenizer attribute"
         self.df = df
         self.config = config
         self.sentence = self.df[self.config.sentence_col].to_numpy()
-        self.tokenizer = tokenizer
+        self.tokenizer = self.config.tokenizer
 
     def __len__(self):
         return len(self.df)
@@ -117,6 +131,7 @@ class ConvertTestDataset(Dataset):
             tokenizer=self.tokenizer,
             max_len=self.config.max_len
         )
-        input_ids = torch.tensor(bert_sentence["input_ids"], dtype=torch.long)
-        attention_mask = torch.tensor(bert_sentence["attention_mask"], dtype=torch.long)
-        return input_ids, attention_mask
+        return {
+            'input_ids': torch.tensor(bert_sentence["input_ids"], dtype=torch.long),
+            'attention_mask': torch.tensor(bert_sentence["attention_mask"], dtype=torch.long),
+        }
